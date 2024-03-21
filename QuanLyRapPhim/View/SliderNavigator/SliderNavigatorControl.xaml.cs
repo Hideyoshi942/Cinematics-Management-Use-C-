79d6c8706c0b5c5030230c5e4b48662567097033
LQThai1603 <lqt1603@gmail.com>
2024-03-21 23:03:50 +0700

    main SliderNavigator

---
+++ QuanLyRapPhim/View/SliderNavigator/SliderNavigatorControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyRapPhim.View.SliderNavigator
+{
+    /// <summary>
+    /// Interaction logic for SliderNavigatorControl.xaml
+    /// </summary>
+    public partial class SliderNavigatorControl : UserControl
+    {
+        bool SliderNavshow = true;
+        public SliderNavigatorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void logo_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (SliderNavshow)
+            {
+                Storyboard storyboard = (Storyboard)this.Resources["Storyboard1"];
+                storyboard.Begin(logo);
+                SliderNavshow = false;
+            }
+            else
+            {
+                Storyboard storyboard = (Storyboard)this.Resources["Storyboard2"];
+                storyboard.Begin(logo);
+                SliderNavshow = true;
+            }
+        }
+    }
+}
--- QuanLyRapPhim/obj/Debug/QuanLyRapPhim_Content.g.i.cs
+++ QuanLyRapPhim/obj/Debug/QuanLyRapPhim_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("icon_menu.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("view/slidernavigator/icon_menu.png")]
 
 
